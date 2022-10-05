9954107ae6545b2d1e59a4ac7abe8498473f8de2
Erkal Alpay <erkalalpay@yandex.com>
2022-10-05 13:09:02 +0300

    patika odev

---
+++ kosullu_Ifadeler/_34artikYil.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class _34artikYil {
+    public static void main(String[] args) {
+        int yil;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Yıl Giriniz: ");
+        yil = input.nextInt();
+
+        if ((yil % 4 == 0) && (yil % 100 != 0) || (yil % 400 ==0)) {
+            System.out.println(yil + " artık yıldır");
+        } else {
+            System.out.println(yil + " artık yıl değildir");
+        }
+    }
+}
