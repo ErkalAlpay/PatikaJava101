253abb893158d7504b3b00b18de1731f17f0287c
Erkal Alpay <erkalalpay@yandex.com>
2022-10-06 19:33:41 +0300

    patika odev

---
+++ Arrays/_71bHarfiYazdirma.java
@@ -0,0 +1,23 @@
+public class _71bHarfiYazdirma {
+    public static void main(String[] args) {
+        String[][] stars = new String[7][4];
+
+        for (int i = 0; i < stars.length; i++) {
+            for (int k = 0; k < stars[i].length; k++) {
+                if (i == 0 || i == 3 || i ==6) {
+                    stars[i][k] = " * ";
+                } else  if (k == 0 || k == 3) {
+                    stars[i][k] = " * ";
+                } else {
+                    stars[i][k] = "   ";
+                }
+            }
+        }
+        for (String[] row : stars) {
+            for (String col : row) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
